a0941f4e7ac106e10e1770b020bcfa92b2552604
user <cwcrazyworld@gmail.com>
2018-07-18 02:05:57 +0300

    Missing host to link to!

--- docs/app/models/user.rb
+++ docs/app/models/user.rb
@@ -1,16 +1,7 @@
 class User < ActiveRecord::Base
-	before_save :email_downcase
-	validates :username, presence: true
-	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 },
-		format: { with: VALID_EMAIL_REGEX },
-		uniqueness: { case_sensitive: false }
-	validates :email, presence: true, length: { minimum: 7 }
-	has_secure_password
-
-	private
-
-	def email_downcase
-		self.email = email.downcase
-	end
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :confirmable
 end
